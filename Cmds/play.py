d82485b86e1c6b0f2e30417b63481fc29ff2105a
MRELEC1R0N <piyushsingh5629@gmail.com>
2023-12-17 21:32:32 +0530

    fix the play command a bit

--- Cmds/play.py
+++ Cmds/play.py
@@ -3,18 +3,27 @@ from bs4 import BeautifulSoup
 import requests
 from discord import FFmpegPCMAudio
 from youtube_dlc import YoutubeDL
+from extra_funtions import measure_time
 
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    @commands.command()
-    async def join(self,ctx):
-        channel = ctx.author.voice.channel
-        await channel.connect()
 
+    @commands.command()
+    async def join(self, ctx):
+        if not ctx.voice_client or not ctx.voice_client.is_connected():
+            channel = ctx.author.voice.channel
+            await channel.connect()
 
     @commands.command(name="play", help="Plays a song from YouTube.")
+    @measure_time
     async def play_song(self, ctx, url):
+        if ctx.author.voice and ctx.author.voice.channel:
+                if not ctx.voice_client or not ctx.voice_client.is_connected():
+                    await ctx.author.voice.channel.connect()
+        else:
+                await ctx.send("You are not connected to a voice channel.")
+                return
         try:
             page = requests.get(url)
             soup = BeautifulSoup(page.content, "lxml")
@@ -36,11 +45,10 @@ class Music(commands.Cog):
 
             with YoutubeDL({"format": "bestaudio/best"}) as ydl:
                 info = ydl.extract_info(f"ytsearch:{search_query}", download=False)
-                video_id = info["entries"][0]["id"]
                 audio_url = info["entries"][0]["formats"][0]["url"]
 
-            voice = await ctx.message.author.voice.channel.connect()
-            player = voice.play(FFmpegPCMAudio(audio_url))
+
+            ctx.voice_client.play(FFmpegPCMAudio(audio_url))
 
             await ctx.send(f"Now playing: {info['entries'][0]['title']}")
 
--- extra_funtions.py
+++ extra_funtions.py
@@ -3,8 +3,19 @@ In this section of code i have stored the extra funtions used by the commands of
 '''
 
 
+import time
+from functools import wraps
 
-
+def measure_time(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = await func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        await args[1].send(f"Command executed in {elapsed_time} seconds.")
+        return result
+    return wrapper
 
 
 
